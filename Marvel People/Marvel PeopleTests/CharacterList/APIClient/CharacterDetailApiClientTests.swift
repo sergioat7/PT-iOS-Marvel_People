7c5e0ab85c492d5fb9893d10d15390a5ca3b6852
Sergio Aragonés <sergioat7@gmail.com>
2020-11-01 22:12:24 +0100

    Created CharacterDetailApiClient tests

---
+++ Marvel People/Marvel PeopleTests/CharacterList/APIClient/CharacterDetailApiClientTests.swift
@@ -0,0 +1,69 @@
+//
+//  CharacterDetailApiClientTests.swift
+//  Marvel PeopleTests
+//
+//  Created by Sergio Aragonés on 01/11/2020.
+//  Copyright (c) 2020 Sergio Aragonés. All rights reserved.
+//
+
+import XCTest
+import RxSwift
+import RxCocoa
+@testable import Marvel_People
+
+class CharacterDetailApiClientTests: XCTestCase {
+    
+    var sut: CharacterDetailApiClient!
+    let disposeBag = DisposeBag()
+    
+    override func setUpWithError() throws {
+        
+        super.setUp()
+        sut = CharacterDetailApiClient()
+    }
+
+    override func tearDownWithError() throws {
+        
+        sut = nil
+        super.tearDown()
+    }
+    
+    func testGetCharacterRight() {
+        
+        var characters: CharactersResponse?
+        
+        let promise = expectation(description: "Search successful")
+        sut.getCharacter(characterId: 1009610)
+        sut
+            .getCharactersDataObserver()
+            .subscribe(onNext: { charactersDataResponse in
+                
+                characters = charactersDataResponse.data.results
+                promise.fulfill()
+            })
+            .disposed(by: disposeBag)
+        wait(for: [promise], timeout: 60)
+        
+        XCTAssertNotNil(characters)
+        XCTAssertTrue(characters!.count == 1)
+    }
+    
+    func testGetCharacterWrong() throws {
+        
+        var error: ErrorResponse?
+        
+        let promise = expectation(description: "Search unsuccessful")
+        sut.getCharacter(characterId: 0)
+        sut
+            .getErrorObserver()
+            .subscribe(onNext: { errorResponse in
+                
+                error = errorResponse
+                promise.fulfill()
+            })
+            .disposed(by: disposeBag)
+        wait(for: [promise], timeout: 60)
+        
+        XCTAssertNotNil(error)
+    }
+}
